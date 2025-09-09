788726dec96dc23eb53d771919f7e2b9845792ce
Lisa Nguyen <80797200+nguyen-lisa@users.noreply.github.com>
2025-09-08 20:06:59 -0400

    Update draw.js

--- draw.js
+++ draw.js
@@ -1,97 +1,44 @@
-var totalShapes = 0;
-var lastSqId = 1;
-var lastCircId = 1; 
+const $ = (sel) => document.querySelector(sel);
+const stage = $("#stage");
+const shapeSel = $("#shape");
+const sizeInput = $("#size");
+const colorInput = $("#color");
+const addBtn = $("#add");
 
-function initDraw() {
-    var menu = document.getElementsByName('option');
+addBtn.addEventListener("click", () => {
+  const type = shapeSel.value;
+  const size = Math.max(10, Math.min(300, parseInt(sizeInput.value || "100", 10)));
+  const color = colorInput.value;
 
-    if(menu.value == "squares"){
-        drawSquare();
-    }
-    else if(menu.value == "circles"){
-        drawCircle();
-    }
-    else if(menu.value == "random") {
-        drawRand();
-    }
-    document.getElementById("r").disabled = true;
-    document.getElementById("s").disabled = true;
-    document.getElementById("c").disabled = true;
-}
+  let el = document.createElement("div");
+  el.className = `shape ${type}`;
+  el.setAttribute("role", "img");
+  el.setAttribute("aria-label", `${type} ${size}px ${color}`);
+  el.title = "Click to remove";
 
+  if (type === "triangle") {
+    // Triangle is CSS borders; size maps to base/height
+    const half = Math.round(size / 2);
+    const height = Math.round(size * 0.86);
+    el.style.width = "0";
+    el.style.height = "0";
+    el.style.borderLeft = `${half}px solid transparent`;
+    el.style.borderRight = `${half}px solid transparent`;
+    el.style.borderBottom = `${height}px solid ${color}`;
+  } else {
+    // Square/Circle: use width/height + background
+    el.style.width = `${size}px`;
+    el.style.height = `${size}px`;
+    el.style.background = color;
+  }
 
+  stage.appendChild(el);
+});
 
-function drawSquare()
-{
-    for (var i = 1; i <= 5; i++)
-    {
-        var parent = document.getElementById("container");
-        var sq = document.createElement("div");
-
-        var x = Math.floor(Math.random() * 401);
-        var y = Math.floor(Math.random() * 401);
-        var r = (Math.random()*255) + 1;
-        var g = (Math.random()*255) + 1;
-        var b = (Math.random()*255) + 1;
-
-        var squareId = "sq" + lastSqId;
-
-        sq.setAttribute("style", "position:absolute;top:" + y + "px;left:" + x + "px;width:50px;height:50px;background-color:rgb(" + r + "," + g + "," + b + ");");
-        sq.setAttribute("id", squareId);
-        sq.setAttribute("onmouseover", "hide(this.id)");
-
-        parent.appendChild(sq);
-        totalShapes += 1;
-        lastSqId += 1;
-    }
-}
-
-function drawCircle()
-{
-    for (var i = 1; i <= 5; i++)
-    {
-        var parent = document.getElementById("container");
-        var circ = document.createElement("div");
-
-        var x = Math.floor(Math.random() * 401);
-        var y = Math.floor(Math.random() * 401);
-        var r = (Math.random()*255) + 1;
-        var g = (Math.random()*255) + 1;
-        var b = (Math.random()*255) + 1;
-
-        var circleId = "circ" + lastCircId;
-
-        circ.setAttribute("style", "position:absolute;top:" + y + "px;left:" + x + "px;width:50px;height:50px;background-color:rgb(" + r + "," + g + "," + b + ");border-radius:50%");
-        circ.setAttribute("id", circleId);
-        circ.setAttribute("onmouseover", "hide(this.id)");
-
-        parent.appendChild(circ);
-        totalShapes += 1;
-        lastCircId += 1;
-    }
-}
-
-//add function to check if to draw circle of square
-function drawRand()
-{
-    var rand = Math.floor(Math.random()*2);
-    if (rand == 0)
-    {
-        drawSquare();
-    }
-    else if (rand == 1)
-    {
-        drawCircle();
-    }
-}
-
-function hide(id)
-{   
-    var child =  document.getElementById(id);
-    child.remove();
-    totalShapes -= 1;
-    if (totalShapes == 0)
-    {
-        alert("Last one!");
-    }
-}
+// Click a shape to remove it
+stage.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.classList.contains("shape")) {
+    target.remove();
+  }
+});
